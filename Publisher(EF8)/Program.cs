e4176ab1e1c5e61593e5ed869450676188a7a4c1
Asem Aly <eng.asem.aly@hotmail.com>
2024-03-13 07:55:49 +0200

    Add project files.

---
+++ Publisher(EF8)/Program.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using PubDomain;
+using PublisherData;
+
+using(PublishContext context =  new PublishContext())
+{
+    context.Database.EnsureCreated();
+}
+//GetAuthors();
+//AddAuthor();
+//GetAuthors();
+AddAuthorWithBooks();
+GetAuthorWithBooks();
+void GetAuthors()
+{
+    using var context = new PublishContext();
+    var authors = context.Authors.ToList();
+    foreach (var author in authors)
+    {
+        Console.WriteLine(author.FirstName + " " + author.LastName);
+    }
+}
+void AddAuthor()
+{
+    var author = new Author() { FirstName = "Assem", LastName = "Aly" };
+    using var context = new PublishContext();
+    context.Authors.Add(author);
+    context.SaveChanges();
+}
+void AddAuthorWithBooks() {
+    var author = new Author()
+    {
+        FirstName = "Mohamed",
+        LastName = "Ahmed",
+        Books = new List<Book>() {
+        new Book(){ Title = "Entity FW Core 8",
+            PublishDate = new DateOnly(2024, 1, 1)},
+        new Book(){Title = "C# Head First", PublishDate = new DateOnly(2020, 1, 1)}
+    }
+    };
+    using var context = new PublishContext();
+    context.Authors.Add(author);
+    context.SaveChanges();
+}
+
+void GetAuthorWithBooks()
+{
+    using var context = new PublishContext();
+    var authors = context.Authors.Include(a => a.Books).ToList();
+    foreach (var author in authors)
+    {
+        Console.WriteLine(author.FirstName + " " + author.LastName);
+        foreach (var book in author.Books)
+        {
+            Console.WriteLine(book.Title);
+        }
+    }
+}
---
+++ PublisherData/PublishContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PubDomain;
+
+namespace PublisherData
+{
+    public class PublishContext : DbContext
+    {
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "Data Source = ASSEM;Initial Catalog = PubDatabase;" +
+                "Integrated Security=True;Encrypt=True;TrustServerCertificate=True;");
+        }
+    }
+}
